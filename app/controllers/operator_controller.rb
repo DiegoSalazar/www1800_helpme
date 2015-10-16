a534a4e23a155ac88e3b45adf509a81cb30c976d
Diego Salazar <diego@greyrobot.com>
2015-10-16 00:10:27 -0400

    added my name to response

--- app/controllers/operator_controller.rb
+++ app/controllers/operator_controller.rb
@@ -3,9 +3,9 @@ class OperatorController < ApplicationController
 
   def answer_call
     people = {
-      ENV['TWILIO_NUMBER'] => 'Diego'
+      ENV['TWILIO_NUMBER'] => 'Test'
     }
-    name = people[params['From']] || 'Monkey'
+    name = people[params['From']] || 'Diego'
 
     response = Twilio::TwiML::Response.new do |r|
       r.Say "Hello #{name}"
